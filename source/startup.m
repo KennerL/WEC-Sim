1b51318e56a07cd979e4238775019737fdcd50eb
KennerL <77023637+KennerL@users.noreply.github.com>
2021-01-07 17:53:28 -0800

    Create startup.m

---
+++ source/startup.m
@@ -0,0 +1,8 @@
+% Define the wecSimPath variable to the location of your local WEC-Sim 
+% source directory as a 'string'. Copy the code in this file and place 
+% it in your'startup.m' file
+
+wecSimPath = 'C:\Users\leilakenner\Documents\MATLAB';
+addpath(genpath(wecSimPath));
+
+
